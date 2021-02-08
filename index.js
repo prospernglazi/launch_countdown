1375e38f7bf43b07bea833892751a8d1f5201de8
Prosper Nglazi <trainoftaught@gmail.com>
2021-02-08 22:33:51 +0200

    add leading sero to time less than 9

--- index.js
+++ index.js
@@ -14,10 +14,19 @@ const updateTimer = setInterval(() => {
 	let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
 	let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-	daysLeft.innerHTML = days;
-	hoursLeft.innerHTML = hours;
-	minutesLeft.innerHTML = minutes;
-	secondsLeft.innerHTML = seconds;
+	console.log(hours);
+
+	if (days <= 9 || hours <= 9 || minutes <= 9 || seconds <= 9) {
+		daysLeft.innerHTML = days.toString().padStart(2, 0);
+		hoursLeft.innerHTML = hours.toString().padStart(2, 0);
+		minutesLeft.innerHTML = minutes.toString().padStart(2, 0);
+		secondsLeft.innerHTML = seconds.toString().padStart(2, 0);
+	} else {
+		daysLeft.innerHTML = days;
+		hoursLeft.innerHTML = hours;
+		minutesLeft.innerHTML = minutes;
+		secondsLeft.innerHTML = seconds;
+	}
 
 	if (timeLeft < 0) {
 		clearInterval(myfunc);
